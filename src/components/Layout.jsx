bf4bc8393cd65486d4aabd046e6c2220f3bf27b8
Tien Hoang Vo <tienhoangvo.dev@gmail.com>
2022-04-11 14:13:53 +0700

    Support PWA with next-pwa

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,27 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Head>
         <title>Task Manager</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
-      <header>
+      <header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <h1>Task Manager</h1>
       </header>
       <main>{children}</main>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
